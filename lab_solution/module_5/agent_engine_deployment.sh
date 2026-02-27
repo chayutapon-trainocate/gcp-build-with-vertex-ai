90dffbed7384395817b5b5b10623e9f105ba9fe1
chayutapon-trainocate <111545043+chayutapon-trainocate@users.noreply.github.com>
2026-02-27 15:37:00 +0700

    Update agent_engine_deployment.sh

--- lab_solution/module_5/agent_engine_deployment.sh
+++ lab_solution/module_5/agent_engine_deployment.sh
@@ -1,6 +1,6 @@
 cd ~/adk_challenge_lab
 adk deploy agent_engine \
-    --display_name="Paint Agent"
+    --display_name="Paint Agent" \
     --project=$(gcloud config get-value project) \
     --region=us-central1 \
     --staging_bucket=gs://$(gcloud config get-value project)-bucket \
